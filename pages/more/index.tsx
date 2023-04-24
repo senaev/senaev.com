08cbc6653062300f6c7fd60721a74328b1e9ce19
Andrei Senaev <senaev@yandex-team.ru>
2023-04-24 19:10:37 +0300

    Add some service links

--- pages/more/index.tsx
+++ pages/more/index.tsx
@@ -18,6 +18,23 @@ export default function Page() {
           </li>
         </ul>
       </li>
+      <li>
+        Administration
+        <ul>
+          <li> 
+            <Link href={'https://analytics.google.com/analytics/web/#/p371912726/reports/intelligenthome'}>Google Analytics</Link>
+          </li>
+          <li> 
+            <Link href={'https://metrika.yandex.ru/dashboard?group=day&period=week&id=93313002'}>Yandex Metrika</Link>
+          </li>
+          <li> 
+            <Link href={'https://partner.yandex.ru/v2/web/campaigns/2349763/'}>Yandex Advertising Network</Link>
+          </li>
+          <li> 
+            <Link href={'https://www.google.com/adsense/new/u/0/pub-5756750483186348/sites/detail/url=senaev.com'}>Google AdSense</Link>
+          </li>
+        </ul>
+      </li>
     </ul>
   );
 }
