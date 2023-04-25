ab264c2e3e9b91bbd81b09a04aa67af60e0b3712
Andrei Senaev <senaev@yandex-team.ru>
2023-04-25 13:52:18 +0300

    resolve error

--- pages/adv/google/standard-formats/index.tsx
+++ pages/adv/google/standard-formats/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- ignore
-const win: Window & {
-    adsbygoogle?: unknown[];
-// eslint-disable-next-line no-restricted-globals, @typescript-eslint/no-explicit-any -- ignore
-} = window as any;
-
 function AdUnit() {
     useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- ignore
+        const win: Window & {
+            adsbygoogle?: unknown[];
+            // eslint-disable-next-line no-restricted-globals, @typescript-eslint/no-explicit-any -- ignore
+        } = window as any;
+
         if (!win.adsbygoogle) {
             win.adsbygoogle = [];
         }
