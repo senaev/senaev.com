cc12862d81eedd1c3c10f60c36202b35ce06450b
Andrei Senaev <senaev@yandex-team.ru>
2023-04-25 19:00:03 +0300

    m

--- components/LayoutWrapper/index.tsx
+++ components/LayoutWrapper/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 
 import { GoogleAdSenseCodeSnippet } from 'components/GoogleAdSenseCodeSnippet';
 import { GoogleAnalyticsCodeSnippet } from 'components/GoogleAnalyticsCodeSnippet';
 import { YandexMetrikaCounter } from 'components/YandexMetrikaCounter';
 import { Navigation } from 'components/Navigation';
+import { HtmlHead } from 'components/HtmlHead';
 
-export function LayoutWrapper({ children }: { children: React.ReactNode; }) {
+export function LayoutWrapper({ children }: { children: React.ReactNode; }): JSX.Element {
     return (
         <>
+            <Head>
+                <HtmlHead />
+            </Head>
             <YandexMetrikaCounter />
             <GoogleAdSenseCodeSnippet />
             <GoogleAnalyticsCodeSnippet />
