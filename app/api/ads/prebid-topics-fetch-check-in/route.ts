6c3e3b4a073dac764dac75d6b4fbf1293b339d45
Andrei Senaev <gsacryfice@gmail.com>
2023-12-03 20:06:15 +0300

    m

--- app/api/ads/prebid-topics-fetch-check-in/route.ts
+++ app/api/ads/prebid-topics-fetch-check-in/route.ts
@@ -5,23 +5,45 @@ import { createCrossOriginHeaders } from 'utils/net/createCrossOriginHeaders';
 export function GET(request: Request) {
     const secBrowsingTopics = request.headers.get('sec-browsing-topics');
 
-    const { searchParams } = new URL(request.url);
+    const { searchParams, hostname } = new URL(request.url);
 
     let parsedTopics: number[] | undefined;
+    let version: string | undefined;
     if (secBrowsingTopics) {
-        const topicsOnly = secBrowsingTopics.split(';')[0];
+        const split = secBrowsingTopics.split(';');
+        const topicsOnly = split[0];
 
         if (topicsOnly?.startsWith('(') && topicsOnly.endsWith(')')) {
             const topics = topicsOnly.slice(1, -1).split(' ')
                 .map(Number);
 
             parsedTopics = topics;
+
+            version = split[1];
         }
     }
 
+    const bidder = searchParams.get('bidder');
+
     return NextResponse.json({
-        request: {
-            bidder: searchParams.get('bidder'),
+        segment: {
+            domain: hostname,
+            topics: parsedTopics?.map((topic) => {
+                const versionSplit = version?.split('=')[1]?.split(':');
+
+                return {
+                    configVersion: versionSplit?.[0],
+                    // TODO: modelVersion и taxonomyVersion могут быть перепутаны
+                    modelVersion: versionSplit?.[1],
+                    taxonomyVersion: versionSplit?.[2],
+                    topic,
+                    version,
+                };
+            }),
+            bidder,
+        },
+        ext: {
+            bidder,
             parsedTopics,
             secBrowsingTopics,
             url: request.url,
