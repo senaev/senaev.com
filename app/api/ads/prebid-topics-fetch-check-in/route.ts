c76494213a3d37ab7a7a8d564182765b63fdb247
Andrei Senaev <gsacryfice@gmail.com>
2023-12-03 19:20:44 +0300

    add headers and body to response

--- app/api/ads/prebid-topics-fetch-check-in/route.ts
+++ app/api/ads/prebid-topics-fetch-check-in/route.ts
@@ -2,9 +2,12 @@ import { NextResponse } from 'next/server';
 
 import { createCrossOriginHeaders } from 'utils/net/createCrossOriginHeaders';
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
     return NextResponse.json({
-        request,
+        request: {
+            headers: [...request.headers.entries()],
+            body: await request.text(),
+        },
     }, {
         headers: {
             ...createCrossOriginHeaders(request),
