b8e06eba18364b8e864f341a45e8ca84922461be
Andrei Senaev <gsacryfice@gmail.com>
2023-12-25 16:23:01 +0300

    add custom html endpoint

---
+++ app/api/ads/html-response-from-query/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+
+
+const EXAMPLE_PLACEHOLDER = '<span style="font-size: 40px;">😅</span>';
+const EXAMPLE_HEADERS = {
+    'First-Header': 'first header value',
+    'Second-Header': 'second header value',
+};
+const exampleHeadersString = Object.entries(EXAMPLE_HEADERS)  
+.map(([key, value]) => `header=${encodeURIComponent(key)}:${encodeURIComponent(value)}`) 
+.join('&');
+
+const EXAMPLE_SEARCH_PARAMS = `?html=${encodeURIComponent(EXAMPLE_PLACEHOLDER)}&${exampleHeadersString}` 
+
+const PLACEHOLDER_TEXT = `
+Use "html" search parameter to place page content and "header" to add required headers to response.
+Example: <code>${EXAMPLE_SEARCH_PARAMS}</code>`;
+
+const MACROS = '{content}';
+ 
+const TEMPLATE = `<!DOCTYPE html> 
+<html>
+<body>
+${MACROS}
+</body> 
+</html>`
+
+
+export function GET(request: Request) {
+    const {searchParams} = new URL(request.url);
+    const htmlContent = searchParams.get('html') ?? PLACEHOLDER_TEXT;
+
+    const responseHeadersFromRequest: Record<string, string> = {};
+    searchParams.getAll('header').forEach((header) => {
+        const [key, value] = header.split(':');
+
+        if (!key || !value) {
+            throw new Error(`Header key of value is wrong in header=[${header}] in url=[${request.url}]`)
+        }
+
+        responseHeadersFromRequest[key] = value;
+    });
+
+    return new NextResponse(TEMPLATE.replace(MACROS, htmlContent), {
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8',
+            ...responseHeadersFromRequest,
+        },
+    });
+}
