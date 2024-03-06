c51c1e3755e3cc6dff3c96fd43e32f9fe7a6abec
Andrei Senaev <gsacryfice@gmail.com>
2024-03-06 22:03:01 +0300

    m

--- app/(iframes)/wedding/page.tsx
+++ app/(iframes)/wedding/page.tsx
@@ -1,73 +1,73 @@
-'use client';
+'use client'
 
-import { useEffect } from 'react';
+import React from 'react'
 
-import { loadScript } from 'utils/Script/loadScript';
-
-type WindowWithRequire = {
-    require?: (scripts: string[]) => void;
-};
+import { loadScript } from 'utils/Script/loadScript'
 
 const IMAGES = [
-    '/wedding/img/0.jpg',
-    '/wedding/img/1.jpg',
-    '/wedding/img/2.jpg',
-    '/wedding/img/3.jpg',
-    '/wedding/img/4.jpg',
-    '/wedding/img/5.jpg',
-    '/wedding/img/6.jpg',
-    '/wedding/img/7.jpg',
+  '/wedding/img/0.jpg',
+  '/wedding/img/1.jpg',
+  '/wedding/img/2.jpg',
+  '/wedding/img/3.jpg',
+  '/wedding/img/4.jpg',
+  '/wedding/img/5.jpg',
+  '/wedding/img/6.jpg',
+  '/wedding/img/7.jpg'
 ]
 
-declare const $: any;
+declare const $: any
 
-let slideIndex = 1;
+let slideIndex = 1
 
-export default function Page() {
-    useEffect(() => {
-        loadScript('http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js')
-            .then(() => {
-                $('.prev').click(() => {
-                    plusSlides(-1);
-                });
-                $('.next').click(() => {
-                    plusSlides(1);
-                });
-            })
-            .catch((error) => {
-                // eslint-disable-next-line no-console -- ignore
-                console.error(error);
-            });
+export default function Page (): JSX.Element {
+  React.useEffect(() => {
+    loadScript('http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js')
+      .then(() => {
+        $('.prev').click(() => {
+          plusSlides(-1)
+        })
+        $('.next').click(() => {
+          plusSlides(1)
+        })
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console -- ignore
+        console.error(error)
+      })
 
-        let timeoutId: any = 0;
-        showSlides(slideIndex);
+    let timeoutId: NodeJS.Timeout | undefined
+    showSlides(slideIndex)
 
-        function plusSlides(n: number) {
-            showSlides(slideIndex += n);    
-        }
+    function plusSlides (n: number): void {
+      showSlides(slideIndex += n)
+    }
 
-        function showSlides(n: number) {
-            console.log('showSlides', n)
-            let i;
-            let slides: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("mySlide") as HTMLCollectionOf<HTMLElement>;
-            if (n > slides.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = slides.length }
-            for (i = 0; i < slides.length; i++) {
-                slides[i]!.style.display = "none";
-            }
+    function showSlides (n: number): void {
+      console.log('showSlides', n)
+      let i
+      const slides: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('mySlide') as HTMLCollectionOf<HTMLElement>
+      if (n > slides.length) { slideIndex = 1 }
+      if (n < 1) { slideIndex = slides.length }
+      for (i = 0; i < slides.length; i++) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        slides[i]!.style.display = 'none'
+      }
 
-            slides[slideIndex - 1]!.style.display = "block";
-            clearTimeout(timeoutId)
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      slides[slideIndex - 1]!.style.display = 'block'
+      clearTimeout(timeoutId)
 
-            timeoutId = setTimeout(function () {
-                plusSlides(1);
-            }, 5000);
-        }
-        
-        return () => clearTimeout(timeoutId);
-    }, []);
+      timeoutId = setTimeout(function () {
+        plusSlides(1)
+      }, 5000)
+    }
 
-    return (
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
+  return (
         <div className="container">
             <link href={'/wedding/style.css'} rel={'stylesheet'} />
             <div className="photoContainer">
@@ -75,21 +75,21 @@ export default function Page() {
                     <div className="photoContainer__grandChild">
                         <div className="slideshowContainer">
                             {
-                                IMAGES.map((url) => <div className="mySlide" style={{
-                                    backgroundImage: `url(${url})`
+                                IMAGES.map((url, i) => <div key={i} className="mySlide" style={{
+                                  backgroundImage: `url(${url})`
                                 }}></div>)
                             }
                             <a className="prev">&#10094;</a>
-                            <a className="next">&#10095;</a> 
+                            <a className="next">&#10095;</a>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
             <div className="textContent">
                 <div role="button" aria-label="Scroll down button" className="scrollDownButton__container" onClick={() => {
-                    $([document.documentElement, document.body]).animate({
-                        scrollTop: $(".textContent").offset().top
-                      }, 500);
+                  $([document.documentElement, document.body]).animate({
+                    scrollTop: $('.textContent').offset().top
+                  }, 500)
                 }}>
                     <div className="scrollDownButton" data-name="icon-arrow_small_bottom">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
@@ -110,12 +110,12 @@ export default function Page() {
                                     src="https://hosting.renderforestsites.com/5699226/1031500/media/8d3aaffc00bcac0d92091e45e5f70eb4.png"
                                     className="  img-loaded RSVP4styled__Img-sc-1c14u6b-31 ldokOu RSVP4styled__Img-sc-1c14u6b-31 ldokOu"
                                     style={{
-                                        width: '128px',
-                                        left: '11px',
-                                        top: '32px',
-                                        transform: 'rotate(0deg)',
-                                        height: 'auto',
-                                        position: 'absolute',
+                                      width: '128px',
+                                      left: '11px',
+                                      top: '32px',
+                                      transform: 'rotate(0deg)',
+                                      height: 'auto',
+                                      position: 'absolute'
                                     }}/>
                                 </figure>
                             </div>
@@ -129,10 +129,11 @@ export default function Page() {
                             <p
                             className="RSVP4styled__Paragraph-sc-1c14u6b-7 WM_GLOBAL_paragraph18 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 eTUclA">
                             <b>Dear family and friends,
-                            </b>Our hearts have officially found their forever home, and we couldn't be more over the moon! We simply
+                            </b>
+                            {`Our hearts have officially found their forever home, and we couldn't be more over the moon! We simply
                             can't wait to celebrate our engagement with all of you. So, mark your calendars for <b>October 6th </b>and
                             get ready for an unforgettable night of love, laughter, and making memories that will warm our hearts for
-                            years to come.&nbsp;&nbsp;
+                            years to come.&nbsp;&nbsp;`}
                             </p>
                         </div>
                         <div className="RSVP4styled__StepsContent-sc-1c14u6b-11 kFJVMc">
@@ -188,8 +189,9 @@ export default function Page() {
                                     Dance &amp; laughter</p>
                                 <p
                                     className="RSVP4styled__StepsParagraph-sc-1c14u6b-18 WM_GLOBAL_paragraph18 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 gKEwVo">
-                                    Here's the time to let loose and hit the dance floor! The DJ will spin an eclectic mix of tunes,
-                                    guaranteed to keep you grooving all night long.</p>
+                                    {`Here's the time to let loose and hit the dance floor! The DJ will spin an eclectic mix of tunes,
+                                    guaranteed to keep you grooving all night long.`}
+                                    </p>
                                 </div>
                             </div>
                             </div>
@@ -209,7 +211,7 @@ export default function Page() {
                                 className="RSVP4styled__DateTitle-sc-1c14u6b-15 WM_GLOBAL_heading20 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 zssBY">
                                 Location</p><a target="_blank"
                                 href="https://maps.google.com/maps?q=Fort+Lauderdale+St,Florida+32091,USA"
-                                className="RSVP4styled__LocationValue-sc-1c14u6b-17 WM_GLOBAL_paragraph18 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 IiUkA">Evergreen
+                                className="RSVP4styled__LocationValue-sc-1c14u6b-17 WM_GLOBAL_paragraph18 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 IiUkA" rel="noreferrer">Evergreen
                                 Feast, Florida, United States</a>
                             </div>
                             </div>
@@ -218,7 +220,8 @@ export default function Page() {
                             The big day is swiftly approaching</p>
                             <h1
                             className="RSVP4styled__Title-sc-1c14u6b-6 WM_GLOBAL_heading42 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 fnNtoH">
-                            Can't wait to see you</h1>
+                            {'Can\'t wait to see you'}
+                            </h1>
                             <p
                             className="RSVP4styled__CardParagraph-sc-1c14u6b-8 WM_GLOBAL_paragraph18 sm-word-wrap EditableContent__styledAsRef-sc-15za8y8-0 eTUclA">
                             Your response is greatly appreciated. Kindly reply by August 30th.</p>
@@ -243,8 +246,9 @@ export default function Page() {
                                     name="options" className="RSVPRadioButtonstyled__Input-sc-1396f4y-2 jKwCPF" value="yes"
                                     />
                                         <label htmlFor="yes"
-                                    className="WM_GLOBAL_paragraph18 RSVPRadioButtonstyled__Label-sc-1396f4y-0 eISTvP">Yes, I wouldn't
-                                    miss it!</label></div>
+                                    className="WM_GLOBAL_paragraph18 RSVPRadioButtonstyled__Label-sc-1396f4y-0 eISTvP">
+                                        {'Yes, I wouldn\'t miss it!'}
+                                        </label></div>
                                 <div className="RSVPRadioButtonstyled__RadioButtonWrap-sc-1396f4y-1 dWXCVc">
                                     <input type="radio" id="no"
                                     name="options" className="RSVPRadioButtonstyled__Input-sc-1396f4y-2 jKwCPF" value="no"/>
@@ -280,9 +284,9 @@ export default function Page() {
                                     <label
                                     className="WM_GLOBAL_paragraph14 RSVPInputstyled__Label-sc-6srjsa-3 fCsLUu" htmlFor="name">Email <span
                                     className="asterisk RSVPInputstyled__Asterisk-sc-6srjsa-2 ihWBnF">*</span></label></div>
-                                    <input  
+                                    <input
                                 type="text" className="WM_GLOBAL_secondary-font RSVPInputstyled__Input-sc-6srjsa-0 bELzdd" value=""/>
-                            </div> 
+                            </div>
                             <div className="   RSVPInputstyled__InputContainer-sc-6srjsa-1 bJwPxJ">
                                 <div className="RSVPInputstyled__LabelWrapper-sc-6srjsa-5 kARbgn">
                                     <label
@@ -290,7 +294,7 @@ export default function Page() {
                                     <span className="asterisk RSVPInputstyled__Asterisk-sc-6srjsa-2 ihWBnF">*</span></label></div>
                                     <input
                                 type="text" className="WM_GLOBAL_secondary-font RSVPInputstyled__Input-sc-6srjsa-0 bELzdd" value=""/>
-                            </div> 
+                            </div>
                             </div><button className="WM_GLOBAL_paragraph14 styled__AdditionalGuestButton-sc-nqpo2f-5 gTqPGZ">+ Additional
                             guest</button>
                             <div className="dynamic-fields styled__DynamicFields-sc-nqpo2f-6 kejHfS">
@@ -310,5 +314,5 @@ export default function Page() {
                 </div>
             </div>
         </div>
-    );
+  )
 }
