bbd53f90ffe3414af3236c0dd4d6e19901e9e83b
Andrei Senaev <gsacryfice@gmail.com>
2024-10-30 10:31:32 +0300

    add better md parser

--- utils/Notes/renderNoteByMarkdownContent.tsx
+++ utils/Notes/renderNoteByMarkdownContent.tsx
@@ -1,17 +1,34 @@
 
 
 import { MarkdownContainer } from 'components/MarkdownContainer';
-import { remark } from 'remark';
-import html from 'remark-html';
+import hljs from 'highlight.js';
+import { Marked } from 'marked';
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import { markedHighlight } from "marked-highlight";
 import { prepareMarkdownContentForNote } from 'utils/prepareMarkdownContentForNote';
+
+const marked = new Marked({
+    gfm: true, 
+    breaks: true,
+    async: true,
+});
+
+marked.use(gfmHeadingId());
+marked.use(markedHighlight({
+	emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      
+        return hljs.highlight(code, { language }).value;
+    }
+  }));
 
 export async function renderNoteByMarkdownContent({ markdownContent }: { markdownContent: string }) {
     const preparedMarkdownContent = await prepareMarkdownContentForNote(markdownContent);
 
     // Use remark to convert markdown into HTML string
-    const processedContent = await remark()
-        .use(html)
-        .process(preparedMarkdownContent);
+    const processedContent = await marked.parse(preparedMarkdownContent);
     const contentHtml = processedContent.toString();
 
     return <MarkdownContainer contentHtml={ contentHtml } />;
