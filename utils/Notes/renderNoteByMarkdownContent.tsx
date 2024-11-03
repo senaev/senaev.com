38348049899820bc85a8390414db301cccd28bc4
Andrei Senaev <gsacryfice@gmail.com>
2024-11-04 00:17:03 +0300

    m

--- utils/Notes/renderNoteByMarkdownContent.tsx
+++ utils/Notes/renderNoteByMarkdownContent.tsx
@@ -1,28 +1,51 @@
 
 
 import { MarkdownContainer } from 'components/MarkdownContainer';
+import GithubSlugger from 'github-slugger';
 import hljs from 'highlight.js';
 import { Marked } from 'marked';
-import { gfmHeadingId } from "marked-gfm-heading-id";
-import { markedHighlight } from "marked-highlight";
+import { markedHighlight } from 'marked-highlight';
 import { prepareMarkdownContentForNote } from 'utils/prepareMarkdownContentForNote';
 
 const marked = new Marked({
-    gfm: true, 
+    gfm: true,
     breaks: true,
     async: true,
 });
 
-marked.use(gfmHeadingId());
+marked.use({
+    renderer: {
+        heading({
+            raw,
+            depth,
+            tokens,
+        }) {
+            const slugger = new GithubSlugger();
+            const text = this.parser.parseInline(tokens);
+            const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+            const id = slugger.slug(raw.trim().split(' ').splice(1).join(' ').toLowerCase());
+
+            return `
+              <h${depth} id="${id}">
+                <a name="${escapedText}" class="MarkdownContainer_anchor" href="#${escapedText}">
+                  #
+                </a>
+                ${text}
+              </h${depth}>`;
+        },
+    },
+});
+
 marked.use(markedHighlight({
-	emptyLangClass: 'hljs',
+    emptyLangClass: 'hljs',
     langPrefix: 'hljs language-',
     highlight(code, lang) {
         const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      
+
         return hljs.highlight(code, { language }).value;
-    }
-  }));
+    },
+}));
 
 export async function renderNoteByMarkdownContent({ markdownContent }: { markdownContent: string }) {
     const preparedMarkdownContent = await prepareMarkdownContentForNote(markdownContent);
