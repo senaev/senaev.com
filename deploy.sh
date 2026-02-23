85c75b5f519706e4d682a2909ac980c0ff0a2616
Andrei Senaev <gsacryfice@gmail.com>
2026-02-23 12:32:35 +0100

    default message

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,21 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 K3S_CLUSTER_DIR="/home/ubuntu/k3s-cluster"
+PROVISIONING_DIR="$K3S_CLUSTER_DIR/provisioning"
+SECRETS_DIR="$K3S_CLUSTER_DIR/secrets"
+VOLUMES_DIR="$K3S_CLUSTER_DIR/volumes"
+
 DEPLOY_HOST="ubuntu@51.250.80.209"
 
 echo "👉 Starting deployment to production server"
 
-# Ensure directory exists on server
-echo "👉 Ensuring k3s-cluster directory exists on server"
-ssh $DEPLOY_HOST "mkdir -p $K3S_CLUSTER_DIR"
+# Ensure directory exists on server and create folder structure (mkdir -p keeps existing content)
+echo "👉 Ensuring k3s-cluster directory and structure exist on server"
+ssh $DEPLOY_HOST "mkdir -p \
+  $PROVISIONING_DIR \
+  $SECRETS_DIR \
+  $VOLUMES_DIR \
+"
 
 echo "👉 Syncing provisioning files to server"
 rsync -avz --delete -e ssh "$SCRIPT_DIR/provisioning/" "$DEPLOY_HOST:$K3S_CLUSTER_DIR/provisioning/"
--- xray/xray-deploy.sh
+++ xray/xray-deploy.sh
@@ -5,14 +5,14 @@ set -e  # Exit on any error
 # ❗️ TODO: move to secrets
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 K3S_CLUSTER_DIR="/home/ubuntu/k3s-cluster"
-XRAY_VOLUME_DIR="$K3S_CLUSTER_DIR/volumes/xray"
+XRAY_SECRET_DIR="$K3S_CLUSTER_DIR/secrets/xray"
 
 # Ensure directory exists on server
 echo "👉 Ensuring k3s-cluster directory exists on server"
-ssh ubuntu@51.250.80.209 "mkdir -p $XRAY_VOLUME_DIR"
+ssh ubuntu@51.250.80.209 "mkdir -p $XRAY_SECRET_DIR"
 
 echo "👉 Uploading xray-config.json to server"
-scp $SCRIPT_DIR/xray-config.json ubuntu@51.250.80.209:$XRAY_VOLUME_DIR/
+scp $SCRIPT_DIR/xray-config.json ubuntu@51.250.80.209:$XRAY_SECRET_DIR/
 
 echo "✅ Config is on the server"
 echo "⚠️ {USER_UUID} and {REALITY_PRIVATE_KEY} should be updated in the config"
